9b368c9c8fb8d03a3643d614a26011b5dd4cf926
michaeleliyahu <mmyy605@gmail.com>
2022-08-23 20:46:48 +0300

    add Categories and categoriyItem to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar"
+import Categories from "../components/Categories"
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
+        <Categories/>
     </div>
   )
 }
