aa3b644f5a2be24af9441c05d92141ec59cb85c7
michaeleliyahu <mmyy605@gmail.com>
2022-08-24 10:42:21 +0300

    create product

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar"
 import Categories from "../components/Categories"
+import Products from "../components/Products"
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
         <Categories/>
+        <Products/>
     </div>
   )
 }
