d11550e4921c923b29d6f1ad020f899e35f75f78
michaeleliyahu <mmyy605@gmail.com>
2022-08-24 15:20:18 +0300

    install react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,37 @@
-// import Home from "./pages/Home"
-// import Product from "./pages/Product";
-// import Login from "./pages/Login";
+import Home from "./pages/Home"
+import Product from "./pages/Product";
+import Login from "./pages/Login";
 import WishList from "./pages/WishList";
-// import Save from "./pages/WishList";
-// import Register from "./pages/Register";
-// import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import ProductList from "./pages/ProductList";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
 
 const App = () => {
-  return <WishList/>;
+  const user = true;
+  return (
+  <Router>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path="/products/:category" element={<ProductList/>}/>
+      <Route path="/products/:id" element={<Product/>}></Route>
+      <Route path="/WishList" element={<WishList/>}/>
+      <Route path="/login" element=
+      {user ?
+        (<Navigate replace to={"/"}/>) 
+        : 
+        (<Login/>)
+      }
+      />
+      <Route path="/register" element={<Register/>}/>
+    </Routes>
+  </Router>
+  );
 };
 
 export default App;
